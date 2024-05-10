7abf65d439cfb0f5a6a08375b4fb823bf740ba29
Albin0825 <albinkarlsson2003@gmail.com>
2024-05-10 21:58:41 +0200

    made cards smaller (300px to 200px), added so isPlaying gets removed automatically if songs ends

--- src/app/spotify/recommendations/songCards.tsx
+++ src/app/spotify/recommendations/songCards.tsx
@@ -12,13 +12,20 @@ export default function SongCards() {
 		const allAudio = document.querySelectorAll('audio');
 		const audioElement = document.getElementById(`preview${index}`) as HTMLAudioElement;
 
+		const onAudioEnd = () => {
+			setIsPlaying(null);
+			audioElement.removeEventListener('ended', onAudioEnd);
+		};
+
 		if (audioElement.paused) {
 			allAudio.forEach((audio) => {
 				audio.pause();
 				audio.currentTime = 0;
+				audio.removeEventListener('ended', onAudioEnd);
 			});
 			audioElement.play();
 			setIsPlaying(index);
+			audioElement.addEventListener('ended', onAudioEnd);
 		} else {
 			audioElement.pause();
 			audioElement.currentTime = 0;
@@ -38,7 +45,7 @@ export default function SongCards() {
 							<label
 								htmlFor={`preview${index}`}
 								className={cn(
-									'relative after:absolute after:top-0 after:h-full after:w-full after:bg-center after:bg-no-repeat after:hover:bg-black/50 after:hover:bg-[url(/img/play.svg)]',
+									'relative after:absolute after:top-0 after:h-full after:w-full after:bg-[length:calc(100%_/_3)_calc(100%_/_3)] after:bg-center after:bg-no-repeat after:hover:bg-black/50 after:hover:bg-[url(/img/play.svg)]',
 									isPlaying === index &&
 										'after:bg-black/50 after:bg-[url(/img/pause.svg)] after:hover:bg-[url(/img/pause.svg)]',
 								)}
@@ -47,8 +54,8 @@ export default function SongCards() {
 								<Image
 									src={item.image.url}
 									alt={`album art of ${item.track.name}`}
-									width={item.image.width}
-									height={item.image.height}
+									width={200}
+									height={200}
 								/>
 							</label>
 							<div className='flex flex-col p-3'>
