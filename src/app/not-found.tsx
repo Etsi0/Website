f8ee62a2377369c05205e447d2f5987128249e47
Etsi0 <albinkarlsson2003@gmail.com>
2024-07-14 21:02:17 +0200

    [add] made it so it's not flush with the header and footer

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,9 @@ import { cn } from '@/lib/util';
 export default function App() {
 	return (
 		<>
-			<section className={cn('flex grow flex-col items-center justify-center text-center')}>
+			<section
+				className={cn('flex grow flex-col items-center justify-center py-16 text-center')}
+			>
 				<div className={cn('relative')}>
 					<Person className='absolute left-1/2 h-full translate-x-[-50%] text-primary-700' />
 					<h1 className={cn('text-[clamp(1rem,50vw,16rem)] text-primary-500')}>404</h1>
@@ -17,7 +19,7 @@ export default function App() {
 				</p>
 				<Link
 					className={cn(
-						'inline-block rounded-md bg-primary-500 p-4 text-input transition-colors hover:bg-primary-50 hover:text-primary-500 focus-visible:bg-primary-50 focus-visible:text-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-primary-500',
+						'inline-block rounded-md bg-primary-500 p-4 text-input transition-colors hover:bg-primary-50 hover:text-primary-500 focus-visible:bg-primary-50 focus-visible:text-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-primary-500'
 					)}
 					href='/'
 				>
