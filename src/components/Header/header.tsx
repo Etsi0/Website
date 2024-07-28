f61e3d1974edbd6a7940d201deb2b5c0431c9316
Etsi0 <albinkarlsson2003@gmail.com>
2024-07-28 15:00:17 +0200

    [fix] made the animation work again and removed incorrect use of aria-selected

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,17 +1,29 @@
 'use client';
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { useState, useEffect, useRef } from 'react';
 import { cn } from '@/lib/util';
+import { useGSAP } from '@gsap/react';
+import { CustomEase } from 'gsap/CustomEase';
+import { usePathname } from 'next/navigation';
+import gsap from 'gsap';
+import Link from 'next/link';
 
+import { Hamburger, PhadoniaLogo } from '@/components/SVGs';
 import { DarkMode } from '@/components/Header/darkMode';
 import { navLinkJson } from '@/json/header/navLinks';
 
-import { Hamburger, PhadoniaLogo } from '@/components/SVGs';
+gsap.registerPlugin(CustomEase);
+CustomEase.create('custom', '0.4, 0, 0.2, 1');
 
 export default function App() {
 	const [isMounted, setMounted] = useState(false);
+	const [isNavOpen, setIsNavOpen] = useState<boolean | undefined>(undefined);
+	const [currentPath, setCurrentPath] = useState('');
 	const pathname = usePathname();
+
+	const gsapContainer = useRef<HTMLDivElement>(null);
+	const navContainer = useRef<HTMLDivElement>(null);
+	const { contextSafe } = useGSAP({ scope: gsapContainer });
+
 	useEffect(() => {
 		if (isMounted) {
 			setCurrentPath(pathname + window.location.hash);
@@ -20,28 +32,48 @@ export default function App() {
 		}
 	}, [isMounted, pathname]);
 
-	const [navOpened, setNavOpened] = useState<boolean | undefined>(undefined);
-	const [currentPath, setCurrentPath] = useState('');
-	function navClick(str: string) {
-		setCurrentPath(str);
-	}
+	const navAnimation = contextSafe(() => {
+		// if isNavOpen true it's about to open, and it it's false it's about to close
+		const navWidth = navContainer.current?.clientWidth;
+		const [start, end] = isNavOpen ? [navWidth, 0] : [0, navWidth];
+
+		gsap.fromTo(
+			'.HeaderMainNav',
+			{
+				x: start,
+			},
+			{
+				ease: 'custom',
+				duration: 0.3,
+				x: end,
+			}
+		);
+	});
+
+	useEffect(() => {
+		if (isNavOpen !== undefined) {
+			navAnimation();
+		}
+	}, [isNavOpen, navAnimation]);
 
 	function Navigation() {
+		function navClick(str: string) {
+			setCurrentPath(str);
+			setIsNavOpen(false);
+		}
+
 		return (
 			<nav
-				className={cn(
-					'pointer-events-auto float-right h-screen min-w-72 translate-x-full overflow-hidden border-l-[1px] bg-body-100 duration-300',
-					(navOpened && 'translate-x-0 transition-transform') ||
-						(navOpened === false && 'transition-transform')
-				)}
 				aria-label='Main Navigation'
+				className='HeaderMainNav pointer-events-auto float-right h-screen min-w-72 translate-x-full overflow-hidden border-l bg-body-100'
+				ref={navContainer}
 			>
 				<ul>
 					{navLinkJson.map((link, i) => (
 						<li key={link.path}>
 							<Link
 								tabIndex={
-									navOpened
+									isNavOpen
 										? currentPath === link.path ||
 											(currentPath === '/' && i === 0)
 											? -1
@@ -49,18 +81,14 @@ export default function App() {
 										: -1
 								}
 								className={cn(
-									`
-										block p-3 text-text-700 
-										aria-selected:bg-primary-500 aria-selected:text-input 
-										[&:not([aria-selected='true'])]:hover:bg-primary-50 [&:not([aria-selected='true'])]:hover:text-primary-600 [&:not([aria-selected='true'])]:focus-visible:bg-primary-50 [&:not([aria-selected='true'])]:focus-visible:text-primary-600 
-										dark:[&:not([aria-selected='true'])]:hover:bg-primary-900 dark:[&:not([aria-selected='true'])]:hover:text-primary-400 dark:[&:not([aria-selected='true'])]:focus-visible:bg-primary-900 dark:[&:not([aria-selected='true'])]:focus-visible:text-primary-400
-									`
+									'block p-3 text-text-700',
+									((currentPath === link.path ||
+										(currentPath === '/' && i === 0)) &&
+										'bg-primary-500 text-input') ||
+										'hover:bg-primary-50 hover:text-primary-600 focus-visible:bg-primary-50 focus-visible:text-primary-600 dark:hover:bg-primary-900 dark:hover:text-primary-400 dark:focus-visible:bg-primary-900 dark:focus-visible:text-primary-400'
 								)}
 								href={link.path}
 								onClick={() => navClick(link.path)}
-								aria-selected={
-									currentPath === link.path || (currentPath === '/' && i === 0)
-								}
 							>
 								{link.name}
 							</Link>
@@ -72,40 +100,38 @@ export default function App() {
 	}
 
 	return (
-		<div className={cn('pointer-events-none fixed z-50 w-full')}>
-			<header className={cn('pointer-events-auto bg-body-100/90 backdrop-blur-xl')}>
-				<div className={cn('mx-auto flex h-16 max-w-7xl items-center justify-between p-3')}>
-					{/*==================================================
-						Page icon
-					==================================================*/}
-					<Link href='/'>
-						<PhadoniaLogo
-							className={cn(
-								'h-8 text-[hsl(from_hsl(var(--body-color-100))_h_s_calc(100_-_l))]'
-							)}
-						/>
-					</Link>
-					{/*==================================================
-						Hamburger icon
-					==================================================*/}
-					<div className={cn('flex h-full')}>
-						<DarkMode />
-						{/* !!! I did not make this hamburgerBtn, https://codepen.io/ainalem/pen/wvKOEMV !!! */}
-						<button
-							className={cn('group aspect-square h-full')}
-							aria-expanded={navOpened}
-							onClick={() => setNavOpened(!navOpened)}
-						>
-							<Hamburger className={cn('text-text-700 group-hover:text-text-300')} />
-						</button>
+		<>
+			<header className='pointer-events-none fixed z-50 w-full' ref={gsapContainer}>
+				<div className='pointer-events-auto bg-body-100/90 backdrop-blur-xl'>
+					<div className='mx-auto flex h-16 max-w-7xl items-center justify-between p-3'>
+						{/*==================================================
+							Page icon
+						==================================================*/}
+						<Link href='/'>
+							<PhadoniaLogo className='h-8 text-[hsl(from_hsl(var(--body-color-100))_h_s_calc(100_-_l))]' />
+						</Link>
+						{/*==================================================
+							Hamburger icon
+						==================================================*/}
+						<div className='flex h-full'>
+							<DarkMode />
+							{/* !!! I did not make this hamburgerBtn, https://codepen.io/ainalem/pen/wvKOEMV !!! */}
+							<button
+								className='group aspect-square h-full'
+								aria-expanded={isNavOpen}
+								onClick={() => setIsNavOpen((current) => !current)}
+							>
+								<Hamburger className='text-text-700 group-hover:text-text-300' />
+							</button>
+						</div>
 					</div>
 				</div>
+				<hr />
+				{/*==================================================
+					nav with all the links in it
+				==================================================*/}
+				<Navigation />
 			</header>
-			<hr />
-			{/*==================================================
-				nav with all the links in it
-			==================================================*/}
-			<Navigation />
-		</div>
+		</>
 	);
 }
