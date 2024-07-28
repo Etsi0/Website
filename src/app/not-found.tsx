6bed3ae5b4a1068d1141cdc0a9a9fd5c88f74bc0
Etsi0 <albinkarlsson2003@gmail.com>
2024-07-28 14:58:30 +0200

    [delete] removed cn because it was used incorrectly and made btn look better

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,21 @@
 import Link from 'next/link';
 
 import { Person } from '@/components/SVGs';
-import { cn } from '@/lib/util';
 
 export default function App() {
 	return (
 		<>
-			<section
-				className={cn('flex grow flex-col items-center justify-center py-16 text-center')}
-			>
-				<div className={cn('relative')}>
+			<section className='flex grow flex-col items-center justify-center py-16 text-center'>
+				<div className='relative'>
 					<Person className='absolute left-1/2 h-full translate-x-[-50%] text-primary-700' />
-					<h1 className={cn('text-[clamp(1rem,50vw,16rem)] text-primary-500')}>404</h1>
+					<h1 className='text-[clamp(1rem,50vw,16rem)] text-primary-500'>404</h1>
 				</div>
-				<p className={cn('max-w-prose p-4')}>
+				<p className='max-w-prose p-4'>
 					Lost your way? No worries, we&apos;ve all been there. Just like our
 					coffee-loving friend here. Let&apos;s guide you home.
 				</p>
 				<Link
-					className={cn(
-						'inline-block rounded-md bg-primary-500 p-4 text-input transition-colors hover:bg-primary-50 hover:text-primary-500 focus-visible:bg-primary-50 focus-visible:text-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-primary-500'
-					)}
+					className='inline-block rounded-md bg-primary-500 px-6 py-3 text-input transition-colors hover:bg-primary-50 hover:text-primary-500 focus-visible:bg-primary-50 focus-visible:text-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-primary-500'
 					href='/'
 				>
 					Home Page
