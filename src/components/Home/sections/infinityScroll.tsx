3cd8e2a132cd4931ac59435445b0654df2560e85
Etsi0 <albinkarlsson2003@gmail.com>
2024-11-17 22:17:27 +0100

    .

--- src/components/Home/sections/infinityScroll.tsx
+++ src/components/Home/sections/infinityScroll.tsx
@@ -24,7 +24,7 @@ export function InfinityScroll({
 	className: string[];
 	duration: number;
 	gap: number;
-	images: (any | StaticImageData)[];
+	images: (string | StaticImageData)[];
 	size: number;
 }) {
 	const [isMounted, setIsMounted] = useState<boolean>(false);
