6dbeb411ef3017de1c29b60a4b6abb1f1db07ff2
Etsi0 <albinkarlsson2003@gmail.com>
2025-01-04 14:15:20 +0100

    .

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -7,11 +7,9 @@ import { usePathname } from 'next/navigation';
 import gsap from 'gsap';
 import Link from 'next/link';
 
-import { PhadoniaLogo } from '@/components/SVGs';
+import { Hamburger, PhadoniaLogo } from '@/components/SVGs';
 import { DarkMode } from '@/components/Header/darkMode';
 import { navLinkJson } from '@/json/header/navLinks';
-import { Button } from '@/components/ui/button';
-import { A } from '@/components/ui/link';
 
 gsap.registerPlugin(CustomEase);
 CustomEase.create('custom', '0.4, 0, 0.2, 1');
@@ -88,8 +86,6 @@ export default function App() {
 		);
 	}
 
-	const hamburgerPathStyle = 'transition-[stroke-dashoffset,_stroke-dasharray] duration-500';
-
 	return (
 		<>
 			<header className='pointer-events-none fixed z-50 w-full' ref={gsapContainer}>
@@ -98,34 +94,18 @@ export default function App() {
 						{/*==================================================
 							Page icon
 						==================================================*/}
-						<A href='/' aria-label='Phadonia'>
+						<Link href='/'>
 							<PhadoniaLogo className='h-8 text-[hsl(from_hsl(var(--body-color-100))_h_s_calc(100_-_l))]' />
-						</A>
+						</Link>
 						{/*==================================================
 							Hamburger icon
 						==================================================*/}
-						<div className='flex h-full gap-[4px]'>
+						<div className='flex h-full'>
 							<DarkMode />
 							{/* !!! I did not make this hamburgerBtn, https://codepen.io/ainalem/pen/wvKOEMV !!! */}
-							<Button
-								className='hamburger-button aspect-square h-full rounded-md'
-								focusable={false}
-								aria-label='Hamburger menu'
-								aria-expanded={isNavOpen}
-								onClick={() => setIsNavOpen((current) => !current)}
-							>
-								<svg className={cn('scale-125 text-text-700')} fill='none' stroke='currentColor' strokeWidth={6.4} viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
-									<path
-										className={cn('path1', hamburgerPathStyle)}
-										d='M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058'
-									/>
-									<path className={cn('path2', hamburgerPathStyle)} d='M 20,50 H 80' />
-									<path
-										className={cn('path3', hamburgerPathStyle)}
-										d='M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942'
-									/>
-								</svg>
-							</Button>
+							<button className='group aspect-square h-full' aria-expanded={isNavOpen} onClick={() => setIsNavOpen((current) => !current)}>
+								<Hamburger className='text-text-700 group-hover:text-text-300' />
+							</button>
 						</div>
 					</div>
 				</div>
